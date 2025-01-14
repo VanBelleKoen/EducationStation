15d63428f7d5b4c997b3638c0936e2436bcf6c17
KoenVanBelle <koen.vanbelle@bignited.be>
2025-01-14 20:29:51 +0100

    Fix: Reduce infinite loop to 20 loops

--- math/tables/main.go
+++ math/tables/main.go
@@ -12,9 +12,11 @@ const (
 	max = 10
 )
 
-var Answer string
-var AmountOfCorrectAnswers int
-var AmountOfTotalAnswers int
+var (
+	Answer                 string
+	AmountOfCorrectAnswers int
+	AmountOfTotalAnswers   int
+)
 
 func Exercises() {
 	fmt.Println("We zullen de tafels tot 10 leren. \n 1. Wil je de tafels rustig leren? \n 2. Wil je de tafels op snelheid leren?")
@@ -23,30 +25,31 @@ func Exercises() {
 
 	switch result.IntValue {
 	case 1:
-		Exercise()
+		for AmountOfTotalAnswers < 20 {
+			Exercise()
+		}
+		fmt.Println("Je hebt", AmountOfCorrectAnswers, "van de", AmountOfTotalAnswers, "vragen goed beantwoord")
 	case 2:
 		fmt.Println("Hoeveel tijd wil je om de tafels te oefenen? \n 1. 30 seconden \n 2. 1 minuut \n 3. 2 minuten, \n 4. 5 minuten")
 		result, _ := fundamentals.FetchInput()
 		WithTimeLimit(result.IntValue)
 	default:
 		fmt.Println("Ongeldige keuze")
-}
+	}
 }
 
 func Exercise() {
-		var correct bool
-		switch rand.Intn(2) {
-		case 0:
-			correct = Multiplication()
-		case 1:
-			correct = Division()
-		}
-		if correct {
-			AmountOfCorrectAnswers++
-		}
-		AmountOfTotalAnswers++
-
-		Exercise()
+	var correct bool
+	switch rand.Intn(2) {
+	case 0:
+		correct = Multiplication()
+	case 1:
+		correct = Division()
+	}
+	if correct {
+		AmountOfCorrectAnswers++
+	}
+	AmountOfTotalAnswers++
 }
 
 func WithTimeLimit(choiceOfTime int) {
